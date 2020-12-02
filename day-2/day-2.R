3914d34095496307c7c400575477975984e83d79
sxmorgan <essexmorgan@gmail.com>
2020-12-02 11:45:22 +0100

    part 2

--- day-2/day-2.R
+++ day-2/day-2.R
@@ -13,3 +13,20 @@ input %>%
     filter(valid) %>%
     magrittr::use_series(X3) %>%
     length()
+
+# new policies, how many now?
+input %>%
+    separate(X1, c('pos1','pos2'), sep = '[-]') %>%
+    mutate_at(vars(pos1, pos2), ~ as.integer(.)) %>%
+    mutate(X2 = str_remove_all(X2,'[:]')) %>%
+    mutate(locs = str_locate_all(X3, X2) %>% 
+               map(., as_tibble)) %>%
+    unnest(locs) %>%
+    select(-end) %>%
+    mutate(potential.match = (start == pos1) | (start == pos2)) %>%
+    filter(potential.match) %>%
+    nest(data = -X3) %>% 
+    mutate(valid = map_lgl(data, ~ nrow(.) == 1)) %>% 
+    filter(valid) %>%
+    magrittr::use_series(X3) %>%
+    length()
