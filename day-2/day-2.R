00d2751b05b92802676a3d7e8318d7e54d4ac367
sxmorgan <essexmorgan@gmail.com>
2020-12-02 12:05:24 +0100

    jakobs case_when solution

--- day-2/day-2.R
+++ day-2/day-2.R
@@ -28,5 +28,31 @@ input %>%
     nest(data = -X3) %>% 
     mutate(valid = map_lgl(data, ~ nrow(.) == 1)) %>% 
     filter(valid) %>%
-    magrittr::use_series(X3) %>%
+    pull(X3) %>%
     length()
+
+# jakob's, for fun :D
+library(here)
+input <- read_delim(here('2020', 'input', '2020-12-02_input.txt'), 
+                    delim=' ', col_names = FALSE) %>% 
+    transmute(word=X3, numbers=X1, letter=X2) %>% 
+    mutate(letter=str_remove(letter, ':')) %>% 
+    separate(numbers, into = c('min', 'max'), sep = '-') %>% 
+    mutate(min=as.numeric(min), max=as.numeric(max))
+
+input <- read_delim('~/Desktop/advent-of-code-20/day-2/input.txt', 
+                    delim = ' ', col_names = FALSE) %>%
+    transmute(word=X3, numbers=X1, letter=X2) %>% 
+    mutate(letter=str_remove(letter, ':')) %>% 
+    separate(numbers, into = c('min', 'max'), sep = '-') %>% 
+    mutate(min=as.numeric(min), max=as.numeric(max))
+
+input %>%
+    mutate(f=str_sub(word, min, min)) %>% 
+    mutate(s=str_sub(word, max, max)) %>% 
+    mutate(valid=case_when(f==letter & s==letter ~ FALSE,
+                           f!=letter & s!=letter ~ FALSE,
+                           f==letter & s!=letter ~ TRUE,
+                           f!=letter & s==letter ~ TRUE)) %>% 
+    pull(valid) %>% 
+    sum
