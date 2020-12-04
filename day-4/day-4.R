416952256db8426ef1bf62ce1a5491e3a1fc7560
sxmorgan <essexmorgan@gmail.com>
2020-12-04 09:05:40 +0100

    part 2 wrong

--- day-4/day-4.R
+++ day-4/day-4.R
@@ -15,3 +15,41 @@ input %>%
                              num.fields==8 ~ TRUE)) %>%
     filter(valid) %>%
     nrow()
+
+# new rules
+data <- input %>%
+    str_split('\n\n') %>%
+    magrittr::extract2(1) %>%
+    as_tibble() %>%
+    mutate_at(vars(value), ~ str_replace_all(., '\n', ' ')) %>%
+    separate(value, ' ', into = paste0('X', seq_len(8))) %>%
+    add_column(ID = paste0('E', seq_len(nrow(.))), .before = 1) %>%
+    gather('var', 'value', -ID) %>%
+    filter(!is.na(value)) %>%
+    arrange(ID, value) %>%
+    select(-var) %>% 
+    separate(value, c('field.name', 'value'), '[:]') %>%
+    spread('field.name','value') %>%
+    select(-V1)
+
+data %>%
+    mutate(byr = ifelse(byr >= 1920 & byr <= 2002, TRUE, FALSE)) %>%
+    mutate(iyr = ifelse(iyr >= 2010 & iyr <= 2020, TRUE, FALSE)) %>%
+    mutate(eyr = ifelse(eyr >= 2020 & eyr <= 2030, TRUE, FALSE)) %>%
+    mutate(unit = str_extract(hgt,'[a-z]')) %>%
+    mutate(hgt = str_remove_all(hgt, '[a-z]')) %>%
+    mutate(hgt = case_when(is.na(unit) ~ FALSE,
+                           unit=='c' & hgt>=150 & hgt<=193 ~ TRUE,
+                           unit=='i' & hgt>=59 & hgt<=76 ~ TRUE, 
+                           (unit=='c' & hgt<150) | (unit=='c' & hgt>193) ~ FALSE,
+                           (unit=='i' & hgt<59) | (unit=='i' & hgt>76) ~ FALSE)) %>%
+    select(-unit) %>%
+    mutate(hcl = str_detect(hcl, '[#][0-9a-zA-Z]')) %>%
+    mutate(ecl = ifelse(ecl %in% c('amb','blu','brn','gry','grn','hzl','oth'),
+                        TRUE, FALSE)) %>%
+    mutate(pid = str_detect(pid, '[0-9]{9}')) %>%
+    select(-cid) %>%
+    mutate(valid = rowSums(.[,2:8])) %>%
+    filter(valid == 7)
+
+# first answer 117 = wrong
