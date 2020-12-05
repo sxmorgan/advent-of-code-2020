0780865d65f23708ec7cd48aebdbfc9af582b328
sxmorgan <essexmorgan@gmail.com>
2020-12-05 23:21:55 +0100

    ;(

--- day-4/day-4.R
+++ day-4/day-4.R
@@ -21,25 +21,27 @@ data <- input %>%
     str_split('\n\n') %>%
     magrittr::extract2(1) %>%
     as_tibble() %>%
-    mutate_at(vars(value), ~ str_replace_all(., '\n', ' ')) %>%
-    mutate(num.fields = str_count(value, ':')) %>%
-    filter(num.fields >= 7) %>%
-    select(-num.fields) %>%
-    separate(value, ' ', into = paste0('X', seq_len(8))) %>%
     add_column(ID = paste0('E', seq_len(nrow(.))), .before = 1) %>%
+    mutate_at(vars(value), ~ str_replace_all(., '\n', ' ')) %>%
+    # mutate(num.fields = str_count(value, ':')) %>%
+    # filter(num.fields >= 7) %>%
+    # select(-num.fields) %>%
+    separate(value, ' ', into = paste0('X', seq_len(8))) %>%
     gather('var', 'value', -ID) %>%
+    select(-var) %>%
     filter(!is.na(value)) %>%
     arrange(ID, value) %>%
-    select(-var) %>% 
     separate(value, c('field.name', 'value'), '[:]') %>%
-    spread('field.name','value') 
+    filter(!str_detect(field.name, 'cid')) %>%
+    spread('field.name','value')
 
-data %>%
-    mutate(byr = ifelse((byr >= 1920 & byr <= 2002 &!is.na(byr)) , TRUE, FALSE)) %>%
-    mutate(iyr = ifelse(iyr >= 2010 & iyr <= 2020 &!is.na(iyr), TRUE, FALSE)) %>%
-    mutate(eyr = ifelse(eyr >= 2020 & eyr <= 2030 &!is.na(eyr), TRUE, FALSE)) %>%
+# data is 230 x 8
+check <- data %>%
+    mutate(byr = (byr >= 1920 & byr <= 2002 & !is.na(byr))) %>%
+    mutate(iyr = (iyr >= 2010 & iyr <= 2020 & !is.na(iyr))) %>%
+    mutate(eyr = (eyr >= 2020 & eyr <= 2030 & !is.na(eyr))) %>%
     mutate(unit = str_extract(hgt,'[a-z]')) %>%
-    mutate(hgt = str_remove_all(hgt, '[a-z]')) %>%
+    mutate(hgt = str_remove_all(hgt, '[cmin]')) %>%
     mutate(hgt = case_when(is.na(unit) ~ FALSE,
                            unit=='c' & hgt>=150 & hgt<=193 ~ TRUE,
                            unit=='i' & hgt>=59 & hgt<=76 ~ TRUE, 
@@ -47,13 +49,27 @@ data %>%
                            (unit=='i' & hgt<59) | (unit=='i' & hgt>76) ~ FALSE)) %>%
     select(-unit) %>%
     filter(!is.na(hcl) & !is.na(ecl) & !is.na(pid)) %>%
-    mutate(hcl = str_detect(hcl, '[#][0-9a-zA-Z]')) %>%
-    mutate(ecl = ifelse(ecl %in% c('amb','blu','brn','gry','grn','hzl','oth'),
-                        TRUE, FALSE)) %>%
+    mutate(hcl = str_detect(hcl, '[#][0-9a-f]{6}')) %>%
+    mutate(ecl = ecl %in% c('amb','blu','brn','gry','grn','hzl','oth')) %>%
     mutate(pid = str_detect(pid, '[0-9]{9}')) %>%
-    select(-cid) %>%
     mutate(valid = rowSums(.[,2:8])) %>%
     filter(valid == 7)
 
-# first answer 117 = wrong
-# still wrong ... 
+# still 117, still wrong ... 
+check <- data %>%
+    filter(byr >= 1920 & byr <= 2002 & !is.na(byr)) %>% #176
+    filter(iyr >= 2010 & iyr <= 2020 & !is.na(iyr)) %>% #149
+    filter(eyr >= 2020 & eyr <= 2030 & !is.na(eyr)) %>% #137
+    mutate(unit = str_extract(hgt,'[a-z]')) %>%
+    mutate(hgt = str_remove_all(hgt, '[cmin]')) %>%
+    mutate(hgt = case_when(is.na(unit) ~ FALSE,
+                           unit=='c' & hgt>=150 & hgt<=193 ~ TRUE,
+                           unit=='i' & hgt>=59 & hgt<=76 ~ TRUE, 
+                           (unit=='c' & hgt<150) | (unit=='c' & hgt>193) ~ FALSE,
+                           (unit=='i' & hgt<59) | (unit=='i' & hgt>76) ~ FALSE)) %>%
+    filter(hgt) %>% #132
+    select(-unit) %>%
+    filter(!is.na(hcl) & !is.na(ecl) & !is.na(pid)) %>% #125
+    filter(str_detect(hcl, '[#][0-9a-f]{6}')) %>% #125
+    filter(ecl %in% c('amb','blu','brn','gry','grn','hzl','oth')) %>% #120
+    filter(str_detect(pid, '[0-9]{9}')) 
