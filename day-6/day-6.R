57d8f51b1406822f44c9e6c72b404047b92c4a51
sxmorgan <essexmorgan@gmail.com>
2020-12-06 10:57:31 +0100

    part 2

--- day-6/day-6.R
+++ day-6/day-6.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(magrittr)
 
 input <- '~/Desktop/advent-of-code-20/day-6/input.txt' %>%
     readLines() %>% 
@@ -8,11 +9,10 @@ input <- '~/Desktop/advent-of-code-20/day-6/input.txt' %>%
     gsub("\n", " ", .) %>%
     as.list()
 
-# part 1
-test <- list('abc','a b c','ab ac','a a a a')
-str <- 'ab ac'
+# part 1 - any
+test <- list('abc','a b c','ab ac','a a a a','b')
 
-unique.answers <- function(str) {
+unique.answers.any <- function(str) {
     letters %>%
         map_dbl(~ str_count(str, .)) %>%
         map_dbl(~ ifelse(. > 0, 1, 0)) %>%
@@ -20,5 +20,20 @@ unique.answers <- function(str) {
 }
 
 input %>%
-    map_dbl(~ unique.answers(.)) %>%
+    map_dbl(~ unique.answers.any(.)) %>%
+    sum()
+
+# part 2 - all; 3579
+unique.answers.all <- function(str) {
+    n.ppl <- str %>%
+        str_count(' ') %>%
+        add(1)
+    letters %>%
+        map_dbl(~ str_count(str, .)) %>%
+        map_lgl(~ . == n.ppl) %>%
+        sum()
+}
+
+input %>% 
+    map_dbl(~ unique.answers.all(.)) %>%
     sum()
