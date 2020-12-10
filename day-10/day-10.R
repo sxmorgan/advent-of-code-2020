c7ef254d24bb3774e04581bdfbdfd98412109aa4
sxmorgan <essexmorgan@gmail.com>
2020-12-10 20:44:08 +0100

    ha ha haha ha

--- day-10/day-10.R
+++ day-10/day-10.R
@@ -3,50 +3,76 @@ library(magrittr)
 
 input <- '~/Desktop/advent-of-code-20/day-10/input.txt' %>%
     read_delim('\n', col_names = FALSE)
-input <- '~/Desktop/advent-of-code-20/day-10/test-input.txt' %>%
-    read_delim('\n', col_names = FALSE)
-input <- '~/Desktop/advent-of-code-20/day-10/test-input-2.txt' %>%
-    read_delim('\n', col_names = FALSE)
+# input <- '~/Desktop/advent-of-code-20/day-10/test-input.txt' %>%
+#     read_delim('\n', col_names = FALSE)
+# input <- '~/Desktop/advent-of-code-20/day-10/test-input-2.txt' %>%
+#     read_delim('\n', col_names = FALSE)
 
 # part 1
-build.adapter <- function(v, start, stop, current) {
-    
-    opt.idx <- which(v %in% seq(current+start, current+stop, 1))
+build.adapter <- function(v, current, target) {
     
     # base case - no options
-    if (length(opt.idx) == 0) return(store)
-        
-    # recursive case 1 - multiple options, take first
-    else if (length(opt.idx) > 1) {
-        val <- min(v[opt.idx])
-        opt.idx <- which(v == val)
-        store <<- c(store, v[opt.idx])
-        build.adapter(v[-opt.idx], start, stop, v[opt.idx]) }
+    # if (length(opt.idx) == 0) return(store)
+    if (length(v) <= 1) {
+        valid <<- valid+1
+        print(valid) }
     
-    # recursive case 2 - one option
     else {
-        store <<- c(store, v[opt.idx])
-        build.adapter(v[-opt.idx], start, stop, v[opt.idx]) }
+        opt.idx <- which(v %in% seq(current+1, current+3, 1))
+        
+        # recursive case 1 - multiple options, take first
+        if (length(opt.idx) > 1) {
+            # val <- min(v[opt.idx])
+            # opt.idx <- which(v == val)
+            # store <<- c(store, v[opt.idx])
+            # val <- v[opt.idx]
+            # build.adapter(v[-opt.idx], 1, 3, v[opt.idx]) 
+            vals <- v[opt.idx]
+            names(vals) <- opt.idx
+            vals %>%
+                sort() %>%
+                iwalk(function(.x, .y) {
+                    curr <- .x
+                    idx <- as.numeric(.y)
+                    build.adapter(v[v > v[idx]], curr, target) })
+        }
+        
+        # recursive case 2 - one option
+        else {
+            # store <<- c(store, v[opt.idx])
+            build.adapter(v[-opt.idx], v[opt.idx], target) }
+    }
 }
 
-calc.diff <- function(v, steps) {
-    v %<>%
-        as_tibble() %>%
-        mutate(lag = lag(value)) %>%
-        filter(!is.na(lag)) %>%
-        mutate(diff = value - lag) %>%
-        group_by(diff) %>%
-        count() %>%
-        filter(diff %in% steps) %>%
-        pull(n)
-}
+# calc.diff <- function(v, steps) {
+#     v %<>%
+#         as_tibble() %>%
+#         mutate(lag = lag(value)) %>%
+#         filter(!is.na(lag)) %>%
+#         mutate(diff = value - lag) %>%
+#         group_by(diff) %>%
+#         count() %>%
+#         filter(diff %in% steps) %>%
+#         pull(n)
+# }
 
-store <<- numeric()
+# store <<- numeric()
+# adapter.order <- input %>%
+#     pull(X1) %>%
+#     build.adapter(1, 3, 0) %>%
+#     c(max(input$X1)+3) %>%
+#     c(0, .)
+# diff.dist <- adapter.order %>%
+#     calc.diff(c(1,3))
+# diff.dist[1]*diff.dist[2]
+
+
+# part 2
+valid <<- 0
+#future::plan(future::multisession)
 adapter.order <- input %>%
     pull(X1) %>%
-    build.adapter(1, 3, 0) %>%
+    build.adapter(0, max(input$X1)+3) %>%
     c(max(input$X1)+3) %>%
     c(0, .)
-diff.dist <- adapter.order %>%
-    calc.diff(c(1,3))
-diff.dist[1]*diff.dist[2]
+print(valid)
